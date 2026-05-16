cb39a3f30a29cdc8ba94c11a408a4bd05b31094d
Sarvinoza8 <altiboyevasarvinoz03@gmail.com>
2026-05-16 21:38:45 +0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+
+if a > b:
+    print("Eng katta son:", a)
+elif b > a:
+    print("Eng katta son:", b)
+else:
+    print("Ikkala son teng")
